52f7192e8cd4175d6a27568083c2299b45dc1c96
MattiaSCupolo <m.cupolo3@studenti.unisa.it>
2026-08-28 17:55:49 +0200

    espansione classe utente

--- WP4/Users.py
+++ WP4/Users.py
@@ -19,7 +19,17 @@ class User(Comunication):
     def __init__(self, role):
         self._role = role
 
+    #inizializzazione identità presso una CA
     def obtainIdentity(self,ca):
         if not self._identity:
-
+            self.kpriv = rsa.generate_private_key(
+                key_size=2048,
+                public_exponent=65537
+            )
+            self.kpub = self.kpriv.public_key()
             self.id = ca.subscribeUser(self , self._role , self._kpub)
+            self._identity = True
+            return
+        print("Identità già inizializzata")
+
+
